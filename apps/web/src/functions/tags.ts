090c206e7c8432bcfd3b5fa90d3a2839f76a849b
Scott <120063861+DdeathDrone@users.noreply.github.com>
2026-03-26 12:15:36 +1100

    tags function implementation

--- apps/web/src/functions/tags.ts
+++ apps/web/src/functions/tags.ts
@@ -1,6 +1,22 @@
 // import { posts, type Post } from "../components/data";
 
-export async function tags(posts: { tags: string; active: boolean }[]) {
+export async function tags(posts: { tags: string; active: boolean }[]): Promise<{name: string, count: number}[]> {
   // TODO: Implement per specification
-  return [];
+
+  return posts
+  .filter((p=>p.active))
+  .sort((a,b)=> a.tags.localeCompare(b.tags))
+  .reduce(
+    (acc, post) =>{
+      const counter = acc.find((c) => c.name = post.tags);
+      if(counter){
+        counter.count++;
+      }
+      else{
+        acc.push({name:post.tags, count: 1});
+      }
+      return acc;
+    },
+    [] as {name: string; count: number}[],
+  );
 }
