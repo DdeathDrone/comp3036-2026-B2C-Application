9192fc14b08c4673390c0db6daec1b9540da3649
Scott <120063861+DdeathDrone@users.noreply.github.com>
2026-03-26 18:05:49 +1100

    Dark mode implementation

--- apps/web/src/components/Layout/AppLayout.tsx
+++ apps/web/src/components/Layout/AppLayout.tsx
@@ -2,6 +2,7 @@ import type { PropsWithChildren } from "react";
 import { Content } from "../Content";
 import { LeftMenu } from "../Menu/LeftMenu";
 import { TopMenu } from "./TopMenu";
+import { ThemeProvider } from "../Themes/ThemeContext";
 
 export async function AppLayout({
   children,
@@ -9,11 +10,13 @@ export async function AppLayout({
 }: PropsWithChildren<{ query?: string }>) {
   return (
     <>
+    <ThemeProvider>
       <LeftMenu />
       <Content>
         <TopMenu query={query} />
         {children}
       </Content>
+    </ThemeProvider>
     </>
   );
 }
--- apps/web/src/components/Themes/ThemeContext.tsx
+++ apps/web/src/components/Themes/ThemeContext.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { PropsWithChildren } from "react";
+import {getCookie, setCookie} from 'cookies-next'
+import { useRouter } from "next/navigation";
+
+
 
 export type Theme = "light" | "dark";
 
@@ -15,3 +20,31 @@ const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 // 1. Create Theme Provider
 // 2. Create useTheme hook
 // 3. Use the provider in your layout
+export const useTheme = () => {
+  return useContext(ThemeContext);
+}
+export const ThemeProvider = ({children}: PropsWithChildren)=>{
+  const router = useRouter();
+
+  const [theme, setTheme] = useState<Theme>(getCookie("theme")?.toString() || "light");
+  const toggleTheme = () =>{
+    //theme == "light" ? document.cookie = "theme=dark" : document.cookie = "theme=light"
+    //setCookie("theme",theme)
+    setTheme((t)=> (
+      t == "light" ? "dark" : "light"
+    ))
+    
+    
+
+    
+  };
+  useEffect(() =>{
+      setCookie("theme", theme.toString())
+      router.refresh();
+    },[theme])
+  return(
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
