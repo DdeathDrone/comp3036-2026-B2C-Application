f5853a4d3d6841dbb9c34bbc6c408b2c763de23f
Scott <120063861+DdeathDrone@users.noreply.github.com>
2026-04-09 13:05:56 +1000

    Rebased filters to use useReducer instead of useState

--- apps/admin/src/components/Filters.tsx
+++ apps/admin/src/components/Filters.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Post } from "@repo/db/data"
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import { AdminList } from "./AdminList";
 
 type Any = any;
@@ -14,11 +14,86 @@ function debounce<T extends (...args: Any[]) => Any>(fn: T, delay = 300) {
   };
 }
 
-export function Filters({posts} : {posts : Post[]}){
-    const [post, setPosts] = useState(posts.sort((a,b) => a.title.localeCompare(b.title)));
-    const [filters, setFilters] = useState<{content?: string, tag?: string, date?: Date, active: boolean}>({content: undefined, tag: undefined, date: undefined, active:false})
-    const [sort, setSort] = useState("title-asc");
 
+type State = {
+  posts: Post[],
+  filters: {content?: string, tag?: string, date?: Date, active: boolean}
+}
+
+	type Action = 
+  | { type: 'FILTER', event: React.ChangeEvent<HTMLInputElement>, postInit: Post[]}
+  | { type: 'SORT', event: React.ChangeEvent<HTMLSelectElement>}
+  | {type: 'CLEAR'}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type){
+    case "FILTER":
+      let result: {content?: string, tag?: string, date?: Date, active: boolean} = state.filters;
+      if(action.event.target.name == "ContentFilter"){
+        result = {content: action.event.target.value.toLowerCase(), tag: state.filters.tag, date: state.filters.date, active: state.filters.active}
+      } 
+      else if(action.event.target.name== "TagFilter"){
+        result = {tag: action.event.target.value.toLowerCase(), content: state.filters.content, date: state.filters.date, active: state.filters.active}
+      } 
+      else if(action.event.target.name == "DateFilter"){
+        result= {date: action.event.target.valueAsDate == null ? undefined : action.event.target.valueAsDate, content: state.filters.content, tag: state.filters.tag, active: state.filters.active }
+      } 
+      else if (action.event.target.name == "ActiveFilter"){
+        result = {active: action.event.target.checked, content: state.filters.content, tag: state.filters.tag, date: state.filters.date}
+      };
+      return{
+        filters: result,
+        posts: action.postInit.filter((p)=> (result.content == undefined ? true : p.title.toLowerCase().includes(result.content) ||
+        p.content.toLowerCase().includes(result.content)) 
+        && (result.tag == undefined ? true : p.tags.toLowerCase().includes(result.tag)) 
+        && (result.date == undefined ? true : p.date.toDateString() == result.date.toDateString())
+        && (result.active ? p.active : true )),
+        
+      };
+    case "SORT":
+      let r;
+      if(action.event.target.value == "title-asc"){
+        r = state.posts.sort((a,b)=> a.title.localeCompare(b.title))
+      }
+      else if(action.event.target.value == "title-desc"){
+        r = state.posts.sort((a,b)=> b.title.localeCompare(a.title))
+      }
+      else if(action.event.target.value == "date-asc"){
+        r = state.posts.sort((a,b) => a.date.valueOf() - b.date.valueOf())
+      }
+      else if(action.event.target.value == "date-desc"){
+        r = state.posts.sort((a,b) => b.date.valueOf() - a.date.valueOf())
+      }
+      return{
+        filters: state.filters,
+        posts: r
+      }
+    case "CLEAR":
+      return{
+        filters: {content: undefined, tag: undefined, date: undefined, active:false},
+        posts: state.posts
+      }
+    default:
+      throw new Error("Action not handled");
+  }
+}
+
+export function Filters({posts} : {posts : Post[]}){
+    //const [post, setPosts] = useState(posts.sort((a,b) => a.title.localeCompare(b.title)));
+    //const [filters, setFilters] = useState<{content?: string, tag?: string, date?: Date, active: boolean}>({content: undefined, tag: undefined, date: undefined, active:false})
+    //const [sort, setSort] = useState("title-asc");
+
+    const init : State = {posts: posts.sort((a,b) => a.title.localeCompare(b.title)), filters: {content: undefined, tag: undefined, date: undefined, active:false}}
+
+    const [state, dispatch] = useReducer(reducer, init);
+    console.log(state.filters);
+
+    const handleDebounce = debounce( // Intended to prevent too many refreshes, but tests hate it sooooooooooo idk
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      dispatch({type: "FILTER", event: event, postInit: posts});
+    });
+
+    /*
     const handleFilter = debounce(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const search = event.target.value;
@@ -68,11 +143,12 @@ export function Filters({posts} : {posts : Post[]}){
     ));       //console.log(filters);
 
       }, [filters]);
+      */
     return (<>
         <form className="flex flex-row mt-1">
 
           <label htmlFor="Sort">Sort By:</label>
-          <select className="border ml-2 mr-2" name="Sort" value={sort} onChange={handleSort}>
+          <select className="border ml-2 mr-2" name="Sort" defaultValue="Title-asc" onChange={(e) => dispatch({type: "SORT", event: e})}>
             <option value="title-asc">Title-asc</option>
             <option value="title-desc">Title-desc</option>
             <option value="date-asc">Date-asc</option>
@@ -80,21 +156,21 @@ export function Filters({posts} : {posts : Post[]}){
           </select>
 
           <label htmlFor="ContentFilter">Filter by Content:</label> 
-          <input className="border ml-2 mr-2" type="text" name="ContentFilter" id="ContentFilter" onChange={handleFilter}></input>
+          <input className="border ml-2 mr-2" type="text" name="ContentFilter" id="ContentFilter" onChange={(e) => dispatch({type: "FILTER", event: e, postInit: posts})}></input>
 
           <label htmlFor="TagFilter">Filter by Tag:</label>
-          <input className="border ml-2 mr-2" type="text" name="TagFilter" id="TagFilter" onChange={handleFilter}></input>
+          <input className="border ml-2 mr-2" type="text" name="TagFilter" id="TagFilter" onChange={(e) => dispatch({type: "FILTER", event: e, postInit: posts})}></input>
 
           <label htmlFor="DateFilter">Filter by Date Created:</label>
-          <input className="border ml-2 mr-2" type="date" name="DateFilter"  id="DateFilter"  onChange={handleFilter}></input>
+          <input className="border ml-2 mr-2" type="date" name="DateFilter"  id="DateFilter"  onChange={(e) => dispatch({type: "FILTER", event: e, postInit: posts})}></input>
 
           <label htmlFor="ActiveFilter">Show Only Active Posts:</label>
-          <input className="ml-2 mt-1"type="checkbox"  id="ActiveFilter" name="ActiveFilter" onChange={handleFilter}></input>
+          <input className="ml-2 mt-1"type="checkbox"  id="ActiveFilter" name="ActiveFilter" onChange={(e) => dispatch({type: "FILTER", event: e, postInit: posts})}></input>
 
-          <button className="border ml-2 mr-2 px-2" onClick={()=>setFilters({content: undefined, tag: undefined, date: undefined, active:false})}>Clear Filters</button>
+          <button className="border ml-2 mr-2 px-2" onClick={(e) => dispatch({type: "CLEAR"})}>Clear Filters</button>
         </form>
         <ul>
-          {post.map((p) => (
+          {state.posts.map((p) => (
             <li key={p.id}>
               <AdminList post={p}></AdminList>
             </li>
