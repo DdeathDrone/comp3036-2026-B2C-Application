cf2ace4353f68e68608b647913ae9079cbce2674
Scott <120063861+DdeathDrone@users.noreply.github.com>
2026-05-01 12:11:19 +1000

    Adjusted database filtering for categories and tags

--- apps/web/src/app/category/[name]/page.tsx
+++ apps/web/src/app/category/[name]/page.tsx
@@ -9,8 +9,9 @@ export default async function Page({
   params: Promise<{ name: string }>;
 }) {
   const { name } = await params;
-  //const postsInCategory = posts.filter(post => post.category.toLowerCase() == name)
-  const postsInCategory = await client.db.post.findMany({where:{category:{ contains:name  }, active:true}, include:{likes:true}}); //TODO: come back to this, might be better solution
+  //TODO: come back to this, might be better solution
+  const posts = await client.db.post.findMany({where:{category:{ contains:name  }, active:true}, include:{likes:true}}); //reduces size of data called from database
+  const postsInCategory = posts.filter(post => post.category.toLowerCase() == name) //filters to the explicit value of the category, ensuring categories with similar names are not conflated
 
   return (
     <AppLayout>
