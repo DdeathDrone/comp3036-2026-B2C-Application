e728f024892b2761cd550f27506947abf2d42151
Scott <120063861+DdeathDrone@users.noreply.github.com>
2026-05-31 13:11:56 +1000

    Database integration of client page

--- apps/web/src/app/api/history/route.ts
+++ apps/web/src/app/api/history/route.ts
@@ -2,14 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 import {client} from "@repo/db/client"
 import { products } from "@repo/db/data";
 
-export async function GET(req : NextRequest)
+export async function GET(req : NextRequest) //TODO: Secure API
 {
-    //const request = await req.json();
-    //const userId = parseInt(request.userId);
-    const userId = 1;
+    const request = await req.json();
+    const userId = parseInt(request.userId);
+
+    //const userId = 1;
 
     //const data = await client.db.product.findMany({where:{history: {some: {userId: userId}}}})
-    const data = await client.db.history.findMany({where:{userId: userId}, include:{Product: true}})
+    const data = await client.db.order.findMany({where:{userId: userId}, include:{OrderItem: {include:{Product: true}}}});
     const response = NextResponse.json(data);
     return response;
 
