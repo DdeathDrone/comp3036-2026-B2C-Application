8c9a99097da0ecb41203010e2036d1720e5bc29d
Scott <120063861+DdeathDrone@users.noreply.github.com>
2026-06-03 14:31:59 +1000

    Improved formatting of product List

--- apps/web/src/components/Product/ListItem.tsx
+++ apps/web/src/components/Product/ListItem.tsx
@@ -4,27 +4,26 @@ import Link from "next/link";
 
 export function ProductListItem({ product }: { product: Product }) {
   
-  /*const date = Intl.DateTimeFormat("en-US", 
-    {
-      year: "numeric",
-      month: "short",
-      day: "2-digit",
-    }
-  ).formatToParts(product.date);
-  */
   return (
     <article
       key={product.id}
-      className="pb-5"
+      className="block h-40 w-350 border-b-2 pt-3 "
       data-test-id={`blog-post-${product.id}`}
       aria-label={product.title}
     >
       {/*<p>{date[2]?.value + " " + date[0]?.value + " " + date[4]?.value } </p>*/}
+      <div className="block w-[200px] float-left mr-10 isolate">
+        <Image src={product.imageUrl} alt={product.title} width={200} height={200}/>
+      </div>
+      <div className="block w-200 float-left mr-10 isolate">
       <Link className="text-xl"href={`/product/${product.urlId}`}>{product.title}</Link>
-      <Image src={product.imageUrl} alt={product.title} width={300} height={300}/>
+      
       <p className="">{product.categories}</p>
       <p>{product.description}</p>
-      <p>Price: ${product.price}</p>
+      </div>
+      <div className="block mr-10 isolate float-right">
+      <p className="text-xl">${product.price}</p>
+      </div>
       {/*<p>#{post.tags.replace(",", " #")}</p>
       <p>{post.views} views {post.likes.length} likes</p>*/}
     </article>
