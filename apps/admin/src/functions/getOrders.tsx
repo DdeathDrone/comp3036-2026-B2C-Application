ff8e25711aa862d601c9bf49b6bd3354560d0fde
Scott <120063861+DdeathDrone@users.noreply.github.com>
2026-06-05 21:23:42 +1000

    Fixed API link for getOrders()

--- apps/admin/src/functions/getOrders.tsx
+++ apps/admin/src/functions/getOrders.tsx
@@ -1,5 +1,6 @@
+"use server"
 export async function getOrders(){
-    const res = await fetch(`http://localhost:3002/api/history`, {method: "GET", credentials: `same-origin`})
+    const res = await fetch(`https://comp3036-2026-b2-c-application-admi.vercel.app/api/history`, {method: "GET", credentials: `same-origin`})
     const parsed = await res.json();
     return parsed;
 }
